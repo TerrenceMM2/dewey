6112f8f7fa3390fc3639bdab91ce0261bfe51bae
Terrence Mahnken <terrencemm2@gmail.com>
2020-03-30 22:49:35 -0500

    sets up express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Middleware Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.listen(PORT, () => console.log(`Server listening @ http://localhost:${PORT}`));
+
+module.exports = app;
