38836b29a7ec48875261cf6308093aa569b45041
Terrence Mahnken <terrencemm2@gmail.com>
2020-03-30 23:34:53 -0500

    edits server.js file per AWS documentation

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const PORT = 5000;
+var port = process.env.PORT || 3000;
 
 // Middleware Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.listen(PORT, () => console.log(`Server listening @ http://localhost:${PORT}`));
+var server = app.listen(port, function () {
+    console.log('Server running at http://127.0.0.1:' + port + '/');
+});
 
 module.exports = app;
