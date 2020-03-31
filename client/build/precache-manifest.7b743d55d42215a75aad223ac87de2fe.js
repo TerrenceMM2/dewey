0f28b73f465702e81002a3b46cf024a0c9774208
Terrence Mahnken <terrencemm2@gmail.com>
2020-03-30 23:17:17 -0500

    updates elastic beanstalk configuration file and build react app

---
+++ client/build/precache-manifest.7b743d55d42215a75aad223ac87de2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146ab1e0565cc13a663491a69fd632a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93d557fa37b6afab614d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a7667d062dfb6642739",
+    "url": "/static/js/2.e2ef05c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2ef05c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d557fa37b6afab614d",
+    "url": "/static/js/main.27837fba.chunk.js"
+  },
+  {
+    "revision": "4769b01184c635b2d917",
+    "url": "/static/js/runtime-main.aa4083ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
