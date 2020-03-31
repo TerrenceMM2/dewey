bf2b0bc6aa30df263ca14d1644fe2c59f6c19bfc
Alec Down <alecdown@gmail.com>
2020-03-31 13:55:46 -0500

    adds compression

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+require('dotenv').config();
 const path = require("path");
+const morganConfig = require('./config/morganConfig');
 const app = express();
 const PORT = 5000;
+const compression = require('compression')
+
+// Server compression
+app.use(compression());
 
 // Middleware Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+// Morgan logging
+app.use(morganConfig);
+
 app.get('/test', (req, res) => {
-    res.json({msg: 'Hello from the back on AWS!'});
+    res.json({ msg: 'Hello from the back on AWS!' });
 });
 
 app.listen(PORT, () => {
