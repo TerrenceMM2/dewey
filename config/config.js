7f0ce469565fa9ee2ff2d2b0da31a30bcc31958e
Alec Down <alecdown@gmail.com>
2020-03-31 20:43:34 -0500

    initial server structure

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_DATABASE,
+    host: process.env.DB_DEV_HOST,
+    dialect: 'mysql',
+    logging: false
+  },
+  staging: {
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_DATABASE,
+    host: process.env.DB_TEST_HOST,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_DATABASE,
+    host: process.env.DB_PROD_HOST,
+    dialect: 'mysql',
+    logging: false
+  }
+};
--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 const express = require('express');
 require('dotenv').config();
-const path = require("path");
+const path = require('path');
 const { morganConfig } = require('./config/morganConfig');
 const app = express();
 const PORT = 5000;
 const compression = require('compression');
-
+const chalk = require('chalk');
+const cors = require('cors');
 
 // Server compression
 app.use(compression());
 
+// Enable CORS
+app.use(cors());
+
 // Middleware Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Morgan logging
 app.use(morganConfig);
 
-app.get('/test', (req, res) => {
-    res.json({ msg: 'Hello from the back on AWS!' });
-});
+// Routes
+app.get('/test', (req, res) => res.json({ msg: 'Hello from the back on AWS!' }));
 
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}.`);
+// Models
+const db = require('./models');
+
+db.sequelize.sync({ alter: true }).then(() => {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.listen(PORT, () => console.log(`Server running on port ${chalk.green.bold(PORT)}!`));
 });
