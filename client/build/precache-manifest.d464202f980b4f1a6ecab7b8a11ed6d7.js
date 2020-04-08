a82ec100c2a92de882bd7bf0cd9cb0b26ad766f3
Terrence Mahnken <terrencemm2@gmail.com>
2020-04-07 20:43:47 -0500

    deployment ready

---
+++ client/build/precache-manifest.d464202f980b4f1a6ecab7b8a11ed6d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ec38c01608511b1e6fd16533e2a4a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13e40b363fa84b18d7a",
+    "url": "/static/css/main.1438e4a2.chunk.css"
+  },
+  {
+    "revision": "52e38ecc3802c09b1c12",
+    "url": "/static/js/2.87230573.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87230573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13e40b363fa84b18d7a",
+    "url": "/static/js/main.6f2c8d97.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
