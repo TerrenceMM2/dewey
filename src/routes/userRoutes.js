ab6b0dc45d97a9ed1e2cf6dcbe63a852e71f612e
Alec Down <alecdown@gmail.com>
2020-04-10 19:38:21 -0500

    extracts user validate() into userService

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -17,8 +17,10 @@ router.post('/api/user/login', UserController.login);
 
 // @route GET api/user/validate
 // @desc tests validation of json web token
-router.get('/api/user/validate', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.status(200).json('Authorized');
-});
+router.get(
+  '/api/user/validate',
+  passport.authenticate('jwt', { session: false }),
+  UserController.validate
+);
 
 export default router;
