19cf1bd84ef67f97da25b0d147b1f22430ef554e
Alec Down <alecdown@gmail.com>
2020-05-19 21:35:09 -0500

    adds navigation and cleans up some minor style issues on various pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,37 @@ import './App.css';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Dashboard } from './pages/Dashboard';
-import { UserAccount } from './pages/UserAccount';
+import { Account } from './pages/Account';
+import { Library } from './pages/Library';
 import { NoMatch } from './pages/NoMatch';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import { PrivateRoute } from './utils/PrivateRoute';
+import Navigation from './components/Navigation/Navigation';
 
 const App = () => {
     return (
         <div className="App">
             <Router>
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                    <PrivateRoute exact path="/account" component={UserAccount} />
-                    <Route exact component={NoMatch} />
-                </Switch>
+                <div
+                    style={{
+                        padding: 30,
+                        paddingBottom: 10,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        height: 'calc(100vh - 126px)'
+                    }}>
+                    <Switch>
+                        <Route exact path="/" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                        <PrivateRoute exact path="/library" component={Library} />
+                        <PrivateRoute exact path="/account" component={Account} />
+                        <Route exact component={NoMatch} />
+                    </Switch>
+                </div>
+                <Navigation />
             </Router>
             <Footer />
         </div>
