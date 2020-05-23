f4815d23ab04a677ca385446aefc557771304f3a
Alec Down <alecdown@gmail.com>
2020-05-23 07:46:58 -0500

    display user profile on settings page

--- client/src/context/contexts/UserContext.js
+++ client/src/context/contexts/UserContext.js
@@ -7,12 +7,18 @@ export const UserContext = createContext();
 export const UserContextProvider = ({ children }) => {
     const [user, dispatch] = useReducer(UserReducer, { loggedIn: false }, () => {
         const token = localStorage.getItem('token');
+
         if (token) {
             const currentTime = Date.now() / 1000;
             try {
-                const { exp } = decode(token);
+                const { exp, email, firstName, lastName } = decode(token);
                 if (exp > currentTime) {
-                    return { loggedIn: true, token };
+                    return {
+                        loggedIn: true,
+                        email,
+                        firstName,
+                        lastName
+                    };
                 } else if (currentTime >= exp) {
                     localStorage.removeItem('token');
                     return { loggedIn: false };
