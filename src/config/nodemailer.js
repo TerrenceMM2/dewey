a0ebe1ce054a62606c7f42d32e170b16e88624ac
Terrence Mahnken <terrencemm2@gmail.com>
2020-06-04 19:30:06 -0500

    removes nodemailer.js from config

--- src/config/nodemailer.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
